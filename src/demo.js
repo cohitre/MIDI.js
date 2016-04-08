cc3b54f505cb0147716bf0813294807bfc87f4c7
cohitre <carlosrr@gmail.com>
2016-04-08 00:56:20 -0700

    Cleaning demo

--- src/demo.js
+++ src/demo.js
@@ -1,23 +1,13 @@
 import Instrument from './instrument';
+import AudioTagPlugin from './plugins/audiotag';
 import acousticGrandPiano from 'soundfonts/acoustic_grand_piano-mp3';
-
-class AudioTagPlugin {
-  play(sound, volume) {
-    const audio = new Audio();
-    audio.src = sound;
-    audio.volume = volume / 127;
-    // audio.loop = true
-    audio.play();
-  }
-}
 
 const instrument = new Instrument.fromMp3Soundfont(acousticGrandPiano);
 const plugin = new AudioTagPlugin();
 
-const notes = ['A0', 'A6', 'A2', 'C1', 'C4', 'D2'];
-
-notes.forEach(function(value, index) {
-  setTimeout(function() {
-    plugin.play(instrument.getNote(value), 127);
-  }, index * 1000)
-});
+['A0', 'A6', 'A2', 'C1', 'C4', 'D2']
+  .forEach(function(note, index) {
+    setTimeout(function() {
+      plugin.play(instrument.getNote(note), 127);
+    }, index * 1000);
+  });
--- src/midi.js
+++
@@ -1,9 +0,0 @@
-export default class Midi {
-  constructor(instrument) {
-    this.instrument = instrument;
-  }
-
-  setVolume(channel, volume, delay) {}
-  noteOn(channel, note, velocity, delay) {}
-  noteOff(channel, note, delay) {}
-}
